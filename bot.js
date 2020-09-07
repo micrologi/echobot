2617b621b21edd422d4018b683ee9d3d20230037
Marlon Andrei <marlon@micrologi.com.br>
2020-09-07 16:37:43 -0300

    Adição de funcionalidade de data e edições no arquivo do bot

--- bot.js
+++ bot.js
@@ -6,17 +6,39 @@ const { ActivityHandler, MessageFactory } = require('botbuilder');
 class EchoBot extends ActivityHandler {
     constructor() {
         super();
+
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text }`;
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
+            
+            var replyText = ``;
+            var data = new Date();
+            var dia     = data.getDate();           // 1-31
+            var dia_sem = data.getDay();            // 0-6 (zero=domingo)
+            var mes     = data.getMonth();          // 0-11 (zero=janeiro)
+            var ano2    = data.getYear();           // 2 dígitos
+            var ano4    = data.getFullYear();       // 4 dígitos
+            var hora    = data.getHours();          // 0-23
+            var min     = data.getMinutes();        // 0-59
+            var seg     = data.getSeconds();        // 0-59
+            var mseg    = data.getMilliseconds();   // 0-999
+            var tz      = data.getTimezoneOffset(); // em minutos
+
+            var str_data = dia + '/' + (mes+1) + '/' + ano4;
+
+            if (context.activity.text == 'data') {
+                replyText = `Data atual: ${ str_data }`;
+            } else {
+                replyText = `Repetindo: ${ context.activity.text }`;
+            }
+            
+            await context.sendActivity(MessageFactory.text(replyText, replyText));    
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
 
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome!';
+            const welcomeText = 'Olá, seja bem vindo!';
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
                     await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
